42b5c32b0dc4f197694fdcea43741c30a7a6ea0a
Kavi Gupta <kavi@berkeley.edu>
2021-08-22 01:00:11 -0400

    some major updates

--- videomaker.py
+++ videomaker.py
@@ -4,19 +4,28 @@ import subprocess
 
 from generate_map import *
 
+LENGTH_LIMIT = 2 * 60 + 20
+
+END_CAR_LENGTH_EACH = 3
+
+
+
 dates = sorted(set(load_statistics()["Day"]))
+
+FRAMERATE = len(dates) / (LENGTH_LIMIT - 2 * END_CAR_LENGTH_EACH)
+
 subprocess.check_call("mkdir -p outputs", shell=True)
 for date in tqdm.tqdm(dates):
     generate_map(date)
 os.system(f"cp outputs/{date}.png outputs/slide_1.png")
 os.system("inkscape --export-type=\"png\" slide.svg --export-filename=outputs/slide_2.png")
 subprocess.check_call(
-    "ffmpeg -framerate 1.3 -pattern_type glob -i 'outputs/20*.png' "
+    f"ffmpeg -framerate {FRAMERATE} -pattern_type glob -i 'outputs/20*.png' "
     "-c:v libx264 -r 30 -pix_fmt yuv420p outputs/main.mp4",
     shell=True,
 )
 subprocess.check_call(
-    f"ffmpeg -framerate 1/5 -pattern_type glob -i "
+    f"ffmpeg -framerate 1/{END_CAR_LENGTH_EACH} -pattern_type glob -i "
     "'outputs/slide*.png' -c:v libx264 -r 30 -pix_fmt yuv420p outputs/end_credits.mp4",
     shell=True,
 )
